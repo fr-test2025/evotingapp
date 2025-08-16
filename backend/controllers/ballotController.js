b667ecb326a3fac2b97593eb9d0245b509b48600
FCruiseIFN636 <frances.cruise@connect.qut.edu.au>
2025-08-16 22:58:51 +1000

    EVA-9 Update Ballot feature added to ballotController.js

--- backend/controllers/ballotController.js
+++ backend/controllers/ballotController.js
@@ -24,3 +24,23 @@ res.status(201).json(ballot);
 res.status(500).json({ message: error.message });
 }
 };
+
+
+//Update Ballot
+const updateBallot = async (req , res ) => {
+const { topic, option1, option2, option3 } = req.body;
+try {
+const ballot = await Ballot.findById(req.params.id);
+if (!ballot) return res.status(404).json({ message: 'Ballot not found' });
+
+ballot.topic = topic || ballot.topic;
+ballot.option1 = option1 || ballot.option1;
+ballot.option2 = option2 || ballot.option2;
+ballot.option3 = option3 || ballot.option3;
+
+const updatedBallot = await ballot.save();
+res.json(updatedBallot);
+} catch (error) {
+res.status(500).json({ message: error.message });
+}
+};
